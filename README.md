49df530289a9b7dcfc4cd3b5f22b42381193ea35
Chase Groom <groom.chase@gmail.com>
2022-01-03 14:24:52 -0800

    boxes+convert

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 ```bash
 curl https://6aqx6cbwj7.execute-api.us-east-1.amazonaws.com/staging/convert6to10 --data @event.json
 ```
+curl 'https://6aqx6cbwj7.execute-api.us-east-1.amazonaws.com/staging/convert6to10' \
+  --data-raw '""' \
+  --compressed
--- src/ProductService.js
+++ src/ProductService.js
@@ -1,5 +1,17 @@
-function getProducts() {
-    return fetch('demo/data/products.json').then(res => res.json()).then(d => d.data);
+async function postData(data = {}) {
+    // Default options are marked with *
+    const response = await fetch('https://6aqx6cbwj7.execute-api.us-east-1.amazonaws.com/staging/convert6to10', {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/json'
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
 }
-
-export {getProducts};
+export { postData };
