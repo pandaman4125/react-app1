de54892d2a15cdca395fb54b2e6c9cb69a981301
Chase Groom <groom.chase@gmail.com>
2022-01-03 20:04:37 -0800

    * delete images * tabs for each table

--- src/App.js
+++ src/App.js
@@ -12,16 +12,29 @@ import 'primeicons/primeicons.css';
 import 'primeflex/primeflex.css';
 
 function App() {
-
+    const [activeIndex, setActiveIndex] = useState(0);
     const [inputConfig, setInputConfig] = useState(null);
     const [xml, setXML] = useState("");
+    const [tables, setTables] = useState({})
     const toast = useRef(null);
 
-    const onClick1 = () => {
+    const click_convert = () => {
         postData({ "config": inputConfig })
-            .then((value) => setXML(value.xml))
+            .then((value) => {
+                setXML(value.xml) ?
+                    console.log(value) :
+                    setTables(value.tables);
+            });
     }
-
+    function tabBuilder(tables) {
+        let result = [];
+        for (var k in tables) {
+            if (tables.hasOwnProperty(k)) {
+                result.push(<TabPanel header={k}></TabPanel>)
+            }
+        }
+        return result;
+    }
     return (
         <div className="datatable-crud-demo surface-card p-4 border-round shadow-2">
             <Toast ref={toast} />
@@ -29,11 +42,13 @@ function App() {
             <div className="text-3xl text-800 font-bold mb-4">SAOS 6x to SAOS 10x Converter</div>
             <InputTextarea rows={10} cols={150} onChange={(e) => setInputConfig(e.target.value)} />
             <br />
-            <Button label="Convert" className="p-button-rounded" onClick={onClick1} />
+            <Button label="Convert" className="p-button-rounded" onClick={click_convert} />
             <br />
             <InputTextarea rows={10} cols={150} value={xml} />
             <br />
-            
+            <TabView activeIndex={activeIndex} onTabChange={(e) => setActiveIndex(e.index)}>
+                {tabBuilder(tables)}
+            </TabView>
         </div>
     );
 }
